eb7e838054e01225e84953995af6d4f729e20286
Foxshadow9645 <103552836+Foxshadow9645@users.noreply.github.com>
2025-11-02 15:00:23 +0100

    Update database.js

--- src/core/database.js
+++ src/core/database.js
@@ -1,1 +1,20 @@
+import mongoose from "mongoose";
+
+/**
+ * Connessione principale al Database MongoDB
+ * Utilizza la variabile MONGODB_URI nel file .env
+ */
+export async function connectDatabase() {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log(`📡 Database connesso a: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("❌ Errore durante la connessione al database:", err.message);
+    process.exit(1);
+  }
+}
 
