8f30e02afa15736a0631c869cdf6e86fe2d78c6a
Foxshadow9645 <103552836+Foxshadow9645@users.noreply.github.com>
2025-11-02 15:00:52 +0100

    Update User.js

--- src/core/models/User.js
+++ src/core/models/User.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  username: String,
+  joinDate: { type: Date, default: Date.now },
+  roles: [String],
+  isStaff: { type: Boolean, default: false },
+  infractions: { type: Number, default: 0 }
+});
+
+export default mongoose.model("User", userSchema);
 
